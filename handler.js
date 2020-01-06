700d2c1e10cd847a7b5435d565f96b55d1167ae2
HyoSung Bidol-Lee <hyosung11@me.com>
2020-01-06 16:57:03 -0500

    write first function

--- handler.js
+++ handler.js
@@ -1,12 +1,18 @@
 'use strict';
 
-module.exports.hello = async event => {
+const emojis = [
+  '😀', '😃', '😄', '😁', '😆', '😍', '🔶', '💎', '🚀'
+];
+
+module.exports.rank = async event => {
+  const rank = event.queryStringParameters.rank;
+  const rankEmoji = emojis[rank >= emojis.length ? emojis.length - 1 : rank];
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
         message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
+        input: rankEmoji,
       },
       null,
       2
